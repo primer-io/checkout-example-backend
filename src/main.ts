5bd04b30cc64468774476dd3685a4f051aa84467
Aladin Taleb <aladin.taleb@gmail.com>
2023-12-20 16:32:54 +0100

    bring everything in a single file

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,73 @@
 import { Hono } from "hono/mod.ts";
 import "std/dotenv/load.ts";
-import { createClientSession } from "./api/createClientSession.ts";
+import { post } from "./utils/post.ts";
+import { primerApiUrl, primerHeaders } from "./api/const.ts";
 
 const app = new Hono();
 
 app.get("/", (c) =>
-  c.text(["Available endpoints:", "", "  POST /client-session"].join("\n")),
+  c.text(["Available endpoints:", "", "  POST /client-session"].join("\n"))
 );
 
-app.post("/client-session", async (c) => {
-  const { amount, currencyCode } = (await c.req.json()) as {
-    amount: number;
-    currencyCode: string;
-  };
+///////////////////////////////////////////
+// Create a client session and send back the client token
+///////////////////////////////////////////
 
-  const res = await createClientSession({
-    amount,
-    currencyCode,
-  });
+app.post("/client-session", async (c) => {
+  const res = await post<ClientSession>(
+    `${primerApiUrl}/client-session`,
+
+    /* ✨ Feel free to update this 👇 */
+    {
+      orderId: crypto.randomUUID(),
+
+      order: {
+        // Line items for this session
+        // If your checkout does not have line items:
+        //  > Pass a single line item with the total amount!
+        lineItems: [
+          {
+            itemId: "shoes-123",
+            description: "Some nice shoes!",
+            amount: 2500, // Amount should be in minor units!
+            quantity: 1,
+          },
+        ],
+      },
+
+      currencyCode: "GBP",
+
+      // emailAddress and billingAddress are required for 3DS
+      customer: {
+        emailAddress: "test@test.com",
+        mobileNumber: "+6588889999",
+        firstName: "John",
+        lastName: "Smith",
+        billingAddress: {
+          firstName: "John",
+          lastName: "Smith",
+          postalCode: "CB94BQ",
+          addressLine1: "47A",
+          countryCode: "CL",
+          city: "Cambridge",
+          state: "Cambridgeshire",
+        },
+      },
+    },
+    /* */
+
+    primerHeaders
+  );
 
   return c.json(res);
 });
+
+type ClientSession = {
+  clientToken: string;
+};
+
+///////////////////////////////////////////
+// Serve
+///////////////////////////////////////////
 
 await Deno.serve(app.fetch);
