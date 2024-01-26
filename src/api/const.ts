a086472bf31d6a6405fa7c56547e81f63b7fd198
Aladin Taleb <aladin.taleb@gmail.com>
2024-01-26 16:20:02 +0100

    chore: prettier

--- src/api/const.ts
+++ src/api/const.ts
@@ -1,9 +1,9 @@
 export const primerHeaders = {
-  accept: "application/json",
-  "content-type": "application/json",
-  "X-API-KEY": process.env.PRIMER_API_KEY || "",
-  "X-API-VERSION": "2.2",
-  "Legacy-Workflows": "false",
+  accept: 'application/json',
+  'content-type': 'application/json',
+  'X-API-KEY': process.env.PRIMER_API_KEY || '',
+  'X-API-VERSION': '2.2',
+  'Legacy-Workflows': 'false',
 };
 
-export const primerApiUrl = "https://api.sandbox.primer.io";
+export const primerApiUrl = 'https://api.sandbox.primer.io';
